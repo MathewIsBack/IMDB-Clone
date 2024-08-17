d64fcb18a3063ff207e5650ec09a7728d91859b6
MathewIsBack <oseghaleokononfua@gmail.com>
2024-08-17 21:48:04 +0100

    new one

---
+++ src/app/about/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4 '>
+        <h1 className="text-2xl font-medium text-amber-600">About</h1>
+        <p>
+        Welcome to our platform, your ultimate destination for everything related to movies, TV shows, and the entertainment industry. We have built this site as a dedicated space for cinephiles, casual viewers, and industry enthusiasts alike.
+        </p>
+
+        <p>
+        Our platform is inspired by the desire to create a community where fans can connect over shared interests and discover new favorites. We have curated an extensive database that includes information on a vast array of titles, from blockbusters to indie gems, ensuring that you have access to everything you need. Each entry is meticulously detailed, offering insights into the cast, crew, plot summaries, reviews, and trivia.
+        </p>
+
+        <p>
+        At the heart of our platform is our community. We encourage users to engage with the content, leave reviews, rate their favorite titles, and participate in discussions with fellow fans. Our goal is to create an inclusive and dynamic environment where everyone’s voice can be heard everyone. 
+        </p>
+    </div>
+  )
+}
