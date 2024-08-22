b6bd363838f4527785d2635f35101ec3d8d80666
MathewIsBack <oseghaleokononfua@gmail.com>
2024-08-22 15:05:55 +0100

    new commit

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -9,7 +9,7 @@ export default async function MoviePage({params}) {
   return (
     <div className="w-full">
         <div className="p-4 md:pt-8 flex flex-col md:flex-row content-center max-w-6xl mx-auto md:space-x-6">
-            <Image src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path || movie.poster_path}`} width={500} height={300} className="rounded-lg">
+            <Image src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path || movie.poster_path}`} width={500} height={300} className="rounded-lg" style={{ maxWidth: '100%', height:"100%" }}>
 
             </Image>
             <div className="p-2">
@@ -20,7 +20,7 @@ export default async function MoviePage({params}) {
                 <p className="mb-3">
                   <span className="font-semibold mr-1">Date Released:</span>
                   {movie.release_date || movie.first_air_date}</p>
-                <p>
+                <p className="mb-3">
                 <span className="font-semibold mr-1">Date Released:</span>
                   {movie.vote_count}
                   </p>
