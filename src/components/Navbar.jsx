7ee2631274611cb956f2f154c71258f320498247
MathewIsBack <oseghaleokononfua@gmail.com>
2024-08-29 16:22:40 +0100

    main commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React , {Suspense} from 'react'
 import NavbarItem from './NavbarItem'
 
 export default function Navbar() {
   return (
     <div className='flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-lg justify-center gap-6'>
+      <Suspense fallback={<div>Loading genre content...</div>}>
         <NavbarItem title='Trending' param='fetchTrending' />
         <NavbarItem title='Top Rated' param='fetchTopRated' />
+      </Suspense>
     </div>
   )
 }
