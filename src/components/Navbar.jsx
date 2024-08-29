11dde79dc7caddc3dc57e67d8df362051d418160
MathewIsBack <oseghaleokononfua@gmail.com>
2024-08-29 16:46:48 +0100

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React , {Suspense} from 'react'
 import NavbarItem from './NavbarItem'
 
+
 export default function Navbar() {
   return (
     <div className='flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-lg justify-center gap-6'>
